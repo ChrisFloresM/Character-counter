3d34a5feb72d4c823b87557ff0a36f07b0cd5c01
Christian <christian.mig.flmo@gmail.com>
2025-08-11 19:52:06 -0600

    Adding word limitation feature

--- src/components/UserInputs.jsx
+++ src/components/UserInputs.jsx
@@ -1,3 +1,4 @@
+const wpmAverage = 250;
 export default function UserInputs(
 	{ inputVal,
 		onChange,
@@ -9,13 +10,19 @@ export default function UserInputs(
 		onMaxChars,
 	}) {
 
+	const charLimitExceeded = isLimit && inputVal.length > maxChars;
+	const readingTime = Math.floor(inputVal.length / 250);
+
 	return (
 		<section className="userInputs">
-			<textarea className="userInputs__text text-3 limitExceeded" placeholder="Start typing here...(or paste your text)" value={inputVal} onChange={onChange}></textarea>
-			<p className="text-4 userInpus__errorMessage">
-				<i className="fa-solid fa-circle-info icon"></i>
-				Limit reached! Your text exceeds {maxChars} characters
-			</p>
+			<textarea className={`userInputs__text text-3 ${charLimitExceeded && "limitExceeded"}`} placeholder="Start typing here...(or paste your text)" value={inputVal} onChange={onChange}></textarea>
+			{
+				charLimitExceeded &&
+				<p className="text-4 userInpus__errorMessage">
+					<i className="fa-solid fa-circle-info icon"></i>
+					Limit reached! Your text exceeds {maxChars} characters
+				</p>
+			}
 			<div className="userInputs__features">
 				<div className="checkboxes">
 					<CheckboxFeature value={excludeSpaces} onChange={onExclude}>Exclude spaces</CheckboxFeature>
@@ -23,7 +30,7 @@ export default function UserInputs(
 					{isLimit &&
 						<input type="number" className="text-4 userInputs__maxVal" value={maxChars} onChange={onMaxChars}/>}
 				</div>
-				<p className="text-4">Approx.reading time: {"<"}1 minute</p>
+				<p className="text-4">Approx.reading time: {"<"}{readingTime} minute</p>
 			</div>
 		</section>
 	)
